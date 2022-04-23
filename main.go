d5c96fc03cad4103970b795956d35dd6973705f4
Amenacliff <amenaclifford@gmail.com>
2022-04-23 13:13:00 +0100

    Implemented LRU Cache with HashMaps & DDL Finished

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
-package LRU_Cache
+package main
+
+import "log"
 
 type Node struct {
 	key  string
-	data interface{}
+	data string
 	prev *Node
 	next *Node
 }
@@ -33,15 +35,24 @@ func (cache *LRUCache) Put(key string, data string) {
 	if cache.head == nil {
 		cache.head = newCacheItem
 		cache.tail = newCacheItem
+	} else {
+		newCacheItem = cache.AddItemToFront(newCacheItem)
 	}
 	cacheMap := cache.cacheMap
 	cacheMap[key] = newCacheItem
 	cache.cacheMap = cacheMap
+
+	if cache.length == cache.maxLength {
+		cache.RemoveLastCacheItem()
+	}
+
+	cache.length = cache.length + 1
+
 }
 
 func (cache *LRUCache) RemoveItem(cacheItem *Node) {
 
-	if cache.head == nil || cacheItem == nil {
+	if cache.head == nil {
 		return
 	}
 
@@ -60,10 +71,60 @@ func (cache *LRUCache) RemoveItem(cacheItem *Node) {
 
 }
 
+func (cache *LRUCache) RemoveLastCacheItem() {
+	lastItem := cache.tail
+	if lastItem != nil {
+		previousItem := lastItem.prev
+		cache.tail = previousItem
+		previousItem.next = nil
+		delete(cache.cacheMap, lastItem.key)
+	}
+	cache.length = cache.length - 1
+}
+
+func (cache *LRUCache) AddItemToFront(cacheItem *Node) *Node {
+	if cache.head == nil {
+		cache.head = cacheItem
+		cache.tail = cacheItem
+		cacheItem.prev = nil
+		cacheItem.next = nil
+	}
+
+	if cache.head != nil {
+		currentHead := cache.head
+		cacheItem.next = currentHead
+		cacheItem.prev = nil
+		currentHead.prev = cacheItem
+		cache.head = cacheItem
+	}
+
+	return cacheItem
+
+}
+
 func (cache *LRUCache) Get(key string) interface{} {
 	if value, ok := cache.cacheMap[key]; ok {
+		if value != cache.head {
+			cache.RemoveItem(value)
+			cache.AddItemToFront(value)
+		}
 		return value.data
+
 	} else {
-		return -1
+		return nil
 	}
+}
+
+func main() {
+	lruCache := LRUCache{}
+	cache := lruCache.InitializeCache(6)
+	cache.Put("boy", "Kevwe")
+	cache.Put("boy1", "Kevwe1")
+	cache.Put("boy2", "Kevwe2")
+	cache.Put("boy3", "Kevwe3")
+	cache.Put("boy4", "Kevwe4")
+	cache.Put("boy5", "Kevwe5")
+	cache.Put("boy6", "Kevwe6")
+	cache.Put("boy7", "Amenannnnaa")
+	log.Println(cache.head.key, cache.tail.key, cache.cacheMap)
 }
