a3cc8acc304667b11bc33dd45dbdc28509d8b5bc
Amenacliff <amenaclifford@gmail.com>
2022-04-24 20:28:19 +0100

    Worked on Testing Length of Cache is kept intact

--- main_test.go
+++ main_test.go
@@ -16,7 +16,6 @@ func TestEvictionPolicy(t *testing.T) {
 
 	if firstItemIn != nil {
 		t.Error("First Item to go in, wasn't evicted from the cache, after size of cache was exceeded")
-
 	}
 
 }
@@ -32,5 +31,19 @@ func TestCacheHitReshuffle(t *testing.T) {
 	firstItemIn := cache.Get(keyToCheck)
 	if firstItemIn != nil && firstItemIn.key != cache.head.key {
 		t.Error("A cache hit is suppose to take the node looked up to the head of the cache's, DLL, it didn't")
+	}
+}
+
+func TestLengthOfList(t *testing.T) {
+	lruCacheInit := &LRUCache{}
+	cache := lruCacheInit.InitializeCache(3)
+	cache.Put("boy1", "AmenaCliff1")
+	cache.Put("boy2", "AmenaCliff2")
+	cache.Put("boy3", "AmenaCliff3")
+	cache.Put("boy4", "AmenaCliff4")
+	cache.Put("boy5", "AmenaCliff5")
+
+	if cache.length > cache.maxLength {
+		t.Error("The Length of the cache should not be more than, the max size of the cache ")
 	}
 }
